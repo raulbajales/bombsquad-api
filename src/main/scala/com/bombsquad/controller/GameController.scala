7dc76138e84254c10f3616e0fa7a30ca01850103
Raul Bajales <raul@jemstep.com>
2020-04-13 19:18:16 -0300

    Addind exception handling for the controller layer

--- src/main/scala/com/bombsquad/controller/GameController.scala
+++ src/main/scala/com/bombsquad/controller/GameController.scala
@@ -3,23 +3,44 @@ package com.bombsquad.controller
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, Forbidden, InternalServerError}
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives.{as, complete, entity, onSuccess, pathPrefix, post, _}
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import akka.util.Timeout
 import com.bombsquad.AppConf
 import com.bombsquad.JsonFormats._
-import com.bombsquad.model.{Game, GameList, User}
+import com.bombsquad.exception.GameDoesNotBelongToUserException
+import com.bombsquad.model.{Game, GameList, GameRequest, User}
 import com.bombsquad.service.GameProtocol
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.Future
 
-case class GameRequest(rows: Int, cols: Int, bombs: Int)
-
 class GameController(delegate: ActorRef[GameProtocol.Command])(implicit val system: ActorSystem[_]) extends LazyLogging {
 
   private implicit val timeout: Timeout = Timeout.create(AppConf.routesAskTimeout)
+
+  implicit def customExceptionHandler: ExceptionHandler =
+    ExceptionHandler {
+      case e: GameDoesNotBelongToUserException =>
+        extractUri { uri =>
+          logger.warn(s"Exception in request to $uri: ${e.getMessage}")
+          complete(HttpResponse(Forbidden))
+        }
+      case e: IllegalArgumentException => {
+        extractUri { uri =>
+          logger.warn(s"Exception in request to $uri: ${e.getMessage}")
+          complete(HttpResponse(BadRequest))
+        }
+      }
+      case e: Throwable => {
+        extractUri { uri =>
+          logger.warn(s"Exception in request to $uri: ${e.getMessage}")
+          complete(HttpResponse(InternalServerError))
+        }
+      }
+    }
 
   val routes: Route = pathPrefix("bombsquad") {
     path("users") {
---
+++ src/main/scala/com/bombsquad/model/GameRequest.scala
@@ -0,0 +1,3 @@
+package com.bombsquad.model
+
+case class GameRequest(rows: Int, cols: Int, bombs: Int)
