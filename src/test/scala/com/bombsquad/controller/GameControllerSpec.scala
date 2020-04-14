ca3e4d82652cea3617872f0ffb7dada3c511f0f7
Raul Bajales <raul@jemstep.com>
2020-04-14 00:24:21 -0300

    More controller tests

--- src/test/scala/com/bombsquad/controller/GameControllerSpec.scala
+++ src/test/scala/com/bombsquad/controller/GameControllerSpec.scala
@@ -8,11 +8,12 @@ import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.{ContentTypes, MessageEntity, StatusCodes}
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import com.bombsquad.JsonFormats._
-import com.bombsquad.model.{Game, GameList, GameRequest, User}
+import com.bombsquad.model.{Cancelled, Game, GameList, GameRequest, Paused, User}
 import com.bombsquad.repository.RepositoryStubs
 import com.bombsquad.service.{GameActor, GameService}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfter, Matchers, WordSpec}
+
 import scala.concurrent.duration._
 
 class GameControllerSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest with BeforeAndAfter with RepositoryStubs {
@@ -43,7 +44,7 @@ class GameControllerSpec extends WordSpec with Matchers with ScalaFutures with S
     request ~> routes ~> check {
       status should === (StatusCodes.Created)
       contentType should === (ContentTypes.`application/json`)
-      entityAs[User] should be(ctxt.user)
+      entityAs[User] should be (ctxt.user)
     }
   }
 
@@ -54,7 +55,7 @@ class GameControllerSpec extends WordSpec with Matchers with ScalaFutures with S
     request ~> routes ~> check {
       status should === (StatusCodes.Created)
       contentType should === (ContentTypes.`text/plain(UTF-8)`)
-      entityAs[String] should be(game._id.toString)
+      entityAs[String] should be (game._id.toString)
     }
   }
 
@@ -63,7 +64,7 @@ class GameControllerSpec extends WordSpec with Matchers with ScalaFutures with S
     request ~> routes ~> check {
       status should === (StatusCodes.OK)
       contentType should === (ContentTypes.`application/json`)
-      entityAs[GameList] should be(ctxt.gameList)
+      entityAs[GameList] should be (ctxt.gameList)
     }
   }
 
@@ -73,10 +74,58 @@ class GameControllerSpec extends WordSpec with Matchers with ScalaFutures with S
     request ~> routes ~> check {
       status should === (StatusCodes.OK)
       contentType should === (ContentTypes.`application/json`)
-      entityAs[String].contains(gameId) should be(true)
-      entityAs[String].contains("board") should be(true)
-      entityAs[String].contains("username") should be(true)
-      entityAs[String].contains("workflow") should be(true)
+      entityAs[String].contains(gameId) should be (true)
+      entityAs[String].contains("board") should be (true)
+      entityAs[String].contains("username") should be (true)
+      entityAs[String].contains("workflow") should be (true)
+    }
+  }
+
+  "be able to pause a running game" in {
+    ctxt.game.start()
+    val gameId = ctxt.game._id.toString
+    val request = Put(s"/bombsquad/users/${ctxt.user.username}/games/$gameId/pause")
+    request ~> routes ~> check {
+      status should === (StatusCodes.OK)
+      contentType should === (ContentTypes.`text/plain(UTF-8)`)
+      entityAs[String] should be (gameId)
+      ctxt.game.workflow.currentState should be (Paused.name)
+    }
+  }
+
+  "be able to cancel a running game" in {
+    ctxt.game.start()
+    val gameId = ctxt.game._id.toString
+    val request = Put(s"/bombsquad/users/${ctxt.user.username}/games/$gameId/cancel")
+    request ~> routes ~> check {
+      status should === (StatusCodes.OK)
+      contentType should === (ContentTypes.`text/plain(UTF-8)`)
+      entityAs[String] should be (gameId)
+      ctxt.game.workflow.currentState should be (Cancelled.name)
+    }
+  }
+
+  "be able to flag a cell in a running game" in {
+    ctxt.game.start()
+    val gameId = ctxt.game._id.toString
+    val request = Put(s"/bombsquad/users/${ctxt.user.username}/games/$gameId/flag?row=3&col=3")
+    request ~> routes ~> check {
+      status should === (StatusCodes.OK)
+      contentType should === (ContentTypes.`text/plain(UTF-8)`)
+      entityAs[String] should be (gameId)
+      ctxt.game.board.cellAt(3, 3).map(_.flagged should be (true))
+    }
+  }
+
+  "be able to uncover a cell in a running game" in {
+    ctxt.game.start()
+    val gameId = ctxt.game._id.toString
+    val request = Put(s"/bombsquad/users/${ctxt.user.username}/games/$gameId/uncover?row=3&col=3")
+    request ~> routes ~> check {
+      status should === (StatusCodes.OK)
+      contentType should === (ContentTypes.`text/plain(UTF-8)`)
+      entityAs[String] should be (gameId)
+      ctxt.game.board.cellAt(3, 3).map(_.covered should be (false))
     }
   }
 }
